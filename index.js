6410ad90a54906b385405c83a6ab462e9783f705
spoorthisona <spoorthispoorthi249@gmail.com>
2024-06-22 11:16:45 +0530

    portfolio added

---
+++ index.js
@@ -0,0 +1,28 @@
+//toggle icon navbar
+
+
+
+
+//scroll sections
+let sections=document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header nav a');
+
+
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+    let top=window.scrollY;
+    let offset=sec.offsetTOP -100;
+    let height=sec.offsetHeight;
+    let id=sec.getAttribute('id')
+
+    if(top >=offset && top < offset + height){
+        navLinks.forEach(links=>{
+            limks.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+        });
+    }
+});
+    let header=document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY >100);
+}
